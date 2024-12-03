961dd3231b469ce398c566d9105b07005bc169fb
don9m1n <ddmkim94@gmail.com>
2024-12-03 14:11:15 +0900

    Feat: 로그인 기능 구현
    - 시큐리티 로그인을 처리하는 `loadUserByUsername()` 구현
    - UserDetails를 구현해서 인증 객체 확장
    - 회원 권한에 작가 권한 추가

---
+++ src/main/java/com/ll/mutbooks/common/security/CustomUserDetails.java
@@ -0,0 +1,35 @@
+package com.ll.mutbooks.common.security;
+
+import com.ll.mutbooks.domain.member.model.Member;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(Member member, Collection<? extends GrantedAuthority>  authorities) {
+        this.member = member;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+}
